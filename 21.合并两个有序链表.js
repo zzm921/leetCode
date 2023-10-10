789c9b7c04dc888ad03f9cc333972f8c10aa9e57
zhangzheming <zhangzheming@cocheer.net>
2023-10-10 18:36:15 +0800

    [feat]提交21题

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -1,64 +1,43 @@
-/**
-将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
-
-示例：
-
-输入：1->2->4, 1->3->4
-输出：1->1->2->3->4->4
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
  */
 
-
+// @lc code=start
 /**
-* Definition for singly-linked list.
-* function ListNode(val) {
-*     this.val = val;
-*     this.next = null;
-* }
-*/
-
-
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
+ * @param {ListNode} list1
+ * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (l1, l2) {
-    if (!l1) return l2
-    if (!l2) return l1
-    let head, pre
-    if (l1.val < l2.val) {
-        head = l1
-        pre = head
-        l1 = l1.next
-    } else {
-        head = l2
-        pre = head
-        l2 = l2.next
+var mergeTwoLists = function (list1, list2) {
+    let result =new ListNode(-1)
+    let p=result
+    if (!list1) {
+        return list2
     }
-    while (l1 && l2) {
-        if (l1.val < l2.val) {
-            pre.next = l1
-            pre = pre.next
-            l1 = l1.next
+    if (!list2) {
+        return list1
+    }
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            p.next = list1
+            list1 = list1.next
         } else {
-            pre.next = l2
-            pre = pre.next
-            l2 = l2.next
+            p.next = list2
+            list2 = list2.next
         }
+        p = p.next
     }
-    pre.next = l1 ? l1 : l2
-    return head
+    p.next = list1 ? list1 : list2
+    return result.next
 };
+// @lc code=end
 
-let a = new ListNode(1)
-let b = new ListNode(2)
-let c = new ListNode(3)
-let d = new ListNode(4)
-a.next = b
-c.next = d
-
-console.log(mergeTwoLists(a, c))
